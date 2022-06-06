a99912a830dbea81b69c27b2db25270179a66f34
akila99g <indurangakga.19@uom.lk>
2022-06-06 10:06:39 +0530

    Code Refactored and TODOs added (object_tracking.py)

--- object_tracking-tutorial/object_tracking.py
+++ object_tracking-tutorial/object_tracking.py
@@ -1,17 +1,24 @@
+import argparse
 import cv2
 import imutils
-import numpy as np
 from object_detection import ObjectDetection
 import math
 import time
 
+# Handle Arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True, help="path to input video")
+ap.add_argument("-o", "--output", required=True, help="path to output video")
+args = vars(ap.parse_args())
+
 # Initialize Object Detection
 od = ObjectDetection()
 
+# Initialize Video Stream Writer
 writer = None
 
-#get video stream
-cap = cv2.VideoCapture("dashCam.mp4")
+# get video stream
+cap = cv2.VideoCapture(args["input"])
 
 # try to determine the total number of frames in the video file
 try:
@@ -25,7 +32,7 @@ except:
     print("[INFO] could not determine # of frames in video")
     print("[INFO] no approx. completion time can be provided")
     total = -1
-# END OF GETTING TOTAL FRAMES
+# END OF GETTING TOTAL NUMBER OF FRAMES
 
 # Initialize count
 count = 0
@@ -34,6 +41,7 @@ center_points_prev_frame = []
 tracking_objects = {}
 track_id = 0
 
+# Processing each frame
 while True:
     ret, frame = cap.read()
     count += 1
@@ -43,9 +51,8 @@ while True:
     # Point current frame
     center_points_cur_frame = []
 
-    #################
+    # setting start time of processing a frame
     start = time.time()
-    #################
 
     # Detect objects on frame
     (class_ids, scores, boxes) = od.detect(frame)
@@ -54,10 +61,11 @@ while True:
         cx = int((x + x + w) / 2)
         cy = int((y + y + h) / 2)
         center_points_cur_frame.append((cx, cy))
-        #print("FRAME N°", count, " ", x, y, w, h)
 
-        # cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # TODO : Warnings simple logic
+        #  #if the rectangle intersecting danger zone lines : give warning
 
     # Only at the beginning we compare previous and current frame
     if count <= 2:
@@ -80,10 +88,15 @@ while True:
 
                 # Update IDs position
                 if distance < 20:
+                    # TODO : Warnings simple logic
+                    #  # get the line passing through pt and pt2
+                    #  # if the line is intersecting danger zone lines : give warning
+
                     tracking_objects[object_id] = pt
                     object_exists = True
                     if pt in center_points_cur_frame:
                         center_points_cur_frame.remove(pt)
+
                     continue
 
             # Remove IDs lost
@@ -99,44 +112,36 @@ while True:
         cv2.circle(frame, pt, 5, (0, 0, 255), -1)
         cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 2)
 
-    # print("Tracking objects")
-    # print(tracking_objects)
-
-
-    # print("CUR FRAME LEFT PTS")
-    # print(center_points_cur_frame)
-
-    #################
+    # setting end time of processing a frame
     end = time.time()
-    #################
 
-
-    # cv2.imshow("Frame", frame)
 # WRITE TO VIDEO FILE
     if writer is None:
-        # initialize our video writer
+        # initialize video writer
         fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-        writer = cv2.VideoWriter("output.avi", fourcc, 30,
-            (frame.shape[1], frame.shape[0]), True)
+        writer = cv2.VideoWriter(args["output"], fourcc, 30, (frame.shape[1], frame.shape[0]), True)
         # some information on processing single frame
         if total > 0:
             elap = (end - start)
             print("[INFO] single frame took {:.4f} seconds".format(elap))
-            print("[INFO] estimated total time to finish: {:.4f}".format(
-                elap * total))
+            print("[INFO] estimated total time to finish: {:.4f}".format(elap * total))
+
     # write the output frame to disk
     writer.write(frame)
 # END OF WRITING FRAME
-    print("Frames left :" , (total-count))
+
+    print("Frames left :", (total-count))
+
     # Make a copy of the points
     center_points_prev_frame = center_points_cur_frame.copy()
 
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
-
 writer.release()
 cap.release()
-cv2.destroyAllWindows()
 
 print("Output video generated")
+
+# References
+# https://pysource.com/2021/10/05/object-tracking-from-scratch-opencv-and-python/
+
+# Command to run
+# python3 object_tracking.py --input input.mp4 --output output.avi
